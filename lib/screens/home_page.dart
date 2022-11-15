0fbd0b00dbb0090da1073a886d94bd99d70e54f3
unknown <info@schedulize.co.ke>
2022-11-15 04:51:17 +0300

    Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:gnews/style/theme.dart' as Style;
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:gnews/bloc/bottom_navbar_bloc.dart';
+import 'package:gnews/style/theme.dart' as Style;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,26 +11,117 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  BottomNavBarBloc? _bottomNavBarBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavBarBloc = BottomNavBarBloc();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(50.0),
-          child: AppBar(
-            backgroundColor: Style.Colors.mainColor,
-            title: Text(
-              "News App",
-              style: TextStyle(
-                color: Colors.white,
-              ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: AppBar(
+          backgroundColor: Style.Colors.mainColor,
+          title: Text(
+            "Global News",
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
+          centerTitle: true,
         ),
-        body: SafeArea(
-          child: StreamBuilder<NavBarItem>(),
+      ),
+      body: SafeArea(
+        child: StreamBuilder<NavBarItem>(
+          stream: _bottomNavBarBloc?.itemStream,
+          initialData: _bottomNavBarBloc?.defaultItem,
+          builder: (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
+            switch (snapshot.data) {
+              case NavBarItem.HOME:
+                return testScreen();
+              case NavBarItem.SOURCES:
+                return testScreen();
+              case NavBarItem.SEARCH:
+                return testScreen();
+              default:
+                return testScreen();
+            }
+          },
         ),
+      ),
+      bottomNavigationBar: StreamBuilder(
+        stream: _bottomNavBarBloc?.itemStream,
+        initialData: _bottomNavBarBloc?.defaultItem,
+        builder: (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
+          return Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30),
+                topLeft: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 0,
+                  blurRadius: 10.0,
+                )
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+              child: BottomNavigationBar(
+                backgroundColor: Colors.white,
+                iconSize: 20.0,
+                unselectedItemColor: Style.Colors.grey,
+                unselectedFontSize: 9.5,
+                selectedFontSize: 9.5,
+                type: BottomNavigationBarType.fixed,
+                fixedColor: Style.Colors.mainColor,
+                currentIndex: snapshot.data!.index,
+                onTap: _bottomNavBarBloc?.pickItem,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(EvaIcons.homeOutline),
+                    activeIcon: Icon(EvaIcons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(EvaIcons.gridOutline),
+                    activeIcon: Icon(EvaIcons.grid),
+                    label: 'Sources',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(EvaIcons.searchOutline),
+                    activeIcon: Icon(EvaIcons.search),
+                    label: 'Search',
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget testScreen() {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const <Widget>[
+          Text("Test Screen"),
+        ],
       ),
     );
   }
